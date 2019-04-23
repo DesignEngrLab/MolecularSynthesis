b660ecd2f3679c2f6654cebddc14ab08cfd75613
Chengxi Yang <ycx923@qq.com>
2019-04-22 17:15:32 -0700

    Change Algorithm Deployed Way

--- plugins/GraphSynth.Search/BFS.cs
+++ plugins/GraphSynth.Search/BFS.cs
@@ -39,6 +39,7 @@ namespace GraphSynth.Search
             _runDirectory = Path.Combine(settings.OutputDirAbs, "BFS");
             if (!Directory.Exists(_runDirectory))
                 Directory.CreateDirectory(_runDirectory);
+            
             Seed = new candidate(OBFunctions.tagconvexhullpoints(settings.seed), settings.numOfRuleSets);
             QueueBFS = new Queue<BFSNode>();
             allNode = new HashSet<string>();

a9ca46325dc1007faef8b1697e229cd75cd0ce92
Chengxi Yang <yangchen@oregonstate.edu>
2019-06-15 17:00:24 -0400

    test est msg

--- plugins/GraphSynth.Search/RandomRuleApplication.cs
+++ plugins/GraphSynth.Search/RandomRuleApplication.cs
@@ -56,7 +56,7 @@ namespace GraphSynth.Search
 
             var port = rnd.Next(1, 65535);
             port = 9999;
-            server = new LearningServer(learnDirectory, _runDirectory, port);
+            server = new LearningServer(learnDirectory, port, _runDirectory);
             client = new MessageClient(port);
         }
 

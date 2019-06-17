fec732596078fda3a1b708fbff2a62324833c7d5
Chengxi Yang <yangchen@oregonstate.edu>
2019-06-16 21:22:15 -0400

    add proptery to carboxlearner

--- output/morfLearn/learner/carboxLearner.py
+++ output/morfLearn/learner/carboxLearner.py
@@ -15,25 +15,27 @@ else:
 	Tensor = torch.FloatTensor
 
 class CarboxLearner(object): 
-	def __init__(self, data_dir, task, model='point'):
+	def __init__(self, data_dir, task, feature="point", property="stiff"):
 		self.data_dir = data_dir
 		self.task = task
-		self.model = model
-		assert model in ['point']
+		self.feature = feature
+		self.property = property
+		assert self.feature in ["point"]
+		assert self.property in ["stiff"]
 		self.valueNet = None
-		if model == 'point':
+		if feature == "point":
 			self.valueNet = util.point.PointNet(self.task)
 		self.data_set = {}
 
 	def predict(self, linkerName):
-		feature_file = os.path.join(self.data_dir, "feature", self.model, linkerName + ".npy")
+		feature_file = os.path.join(self.data_dir, "feature", self.feature, linkerName + ".npy")
 		arr = np.load(feature_file)
 		arr = np.expand_dims(arr, axis=0)
 		est = self.valueNet(Tensor(arr))
 		return est.detach().cpu().numpy().squeeze()
 
 	def addData(self, linkerName):
-		property_file = os.path.join(self.data_dir, "property", self.model, linkerName + ".npy")
+		property_file = os.path.join(self.data_dir, "property", self.property, linkerName + ".npy")
 		self.dataset[linkerName] = np.load(property_file)
 		return self.dataset
 

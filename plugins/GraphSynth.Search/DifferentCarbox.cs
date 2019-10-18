73a6edd24d1b28165aec7b107a5f416c2c69e08e
Chengxi Yang <yangchen@oregonstate.edu>
2019-10-17 21:59:38 -0700

    test

--- plugins/GraphSynth.Search/DifferentCarbox.cs
+++ plugins/GraphSynth.Search/DifferentCarbox.cs
@@ -64,7 +64,7 @@ namespace GraphSynth.Search
 
         protected override void Run()
         {
-            Console.WriteLine("Fall 2019 One Drive.");
+            Console.WriteLine("Fall 2019 One Drive....");
             server.StartOnlineServer();
             client.Connect(10);
 

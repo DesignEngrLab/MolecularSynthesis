d69636b24552497ac297726d5ff9e483b1362522
Matt Campbell <micampbell@users.noreply.github.com>
2020-07-22 15:44:25 -0700

    change file naming

--- MolecularSynthesis.GS.Plugin/TestBenzringInOpenBabel.cs
+++ MolecularSynthesis.GS.Plugin/TestBenzringInOpenBabel.cs
@@ -3,13 +3,15 @@ using System;
 using OpenBabelFunctions;
 using GraphSynth.Search;
 using GraphSynth;
+using System.IO;
 
 namespace TestOpenBabel
 {
     public class TestBenzRing : SearchProcess
     {
         public override string text => "TestBenz";
-
+        public string filename = @"..\..\..\..\test";
+        public string extension = "mol";
         //deault constructor
         public TestBenzRing(GlobalSettings settings) : base(settings)
         {
@@ -19,13 +21,14 @@ namespace TestOpenBabel
         }
         protected override void Run()
         {
-            var resultMol= OBFunctions.designgraphtomol(seedGraph);
+            var resultMol = OBFunctions.designgraphtomol(seedGraph);
             resultMol = OBFunctions.InterStepMinimize(resultMol);
             OBFunctions.updatepositions(seedGraph, resultMol);
-            
+
             var conv = new OBConversion();
-            conv.SetInAndOutFormats("pdb", "mol");
-            conv.WriteFile(resultMol, @"C:\Users\zhang\source\repos\MolecularSynthesis\output\minimize.mol");
+            conv.SetInAndOutFormats("pdb", extension);
+            conv.WriteFile(resultMol, filename + "." + extension);
+            //File.AppendText()
         }
     }
 
